d2e2fee678068793d289b3732ed69c45e6352fe0
tanbirul-arefin <mdtanbirularefin67@gmail.com>
2026-09-12 22:50:38 +0600

    add the service and student

---
+++ src/main/java/com/real/spring_jpa/model/Student.java
@@ -0,0 +1,7 @@
+package com.real.spring_jpa.model;
+
+public class Student {
+    private int id;
+    private String name;
+    private String department;
+}
---
+++ src/main/java/com/real/spring_jpa/service/StudentService.java
@@ -0,0 +1,32 @@
+package com.real.spring_jpa.service;
+
+import com.real.spring_jpa.model.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public void saveStudent(Student student){
+
+    }
+
+    public List<Student> getStudents(){
+        return null;
+    }
+
+    public Student getStudent(int id){
+        return null;
+    }
+
+    public List<Student> getStudentsByDepartment(String name){
+        return null;
+    }
+    public void updateStudent(Student student){
+
+    }
+    public void deleteStudent(int id){
+
+    }
+}
